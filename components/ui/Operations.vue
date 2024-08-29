1d1fa118ab7fecafe6cedc78426e64070c7fd890
fwaszkiewicz <fabianwaszkiewicz09@gmail.com>
2024-08-29 11:28:38 +0200

    feat: Improve authority account for each operation

--- components/ui/Operations.vue
+++ components/ui/Operations.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="w-1/2">
-    <v-expansion-panels bg-color="black">
-      <v-expansion-panel>
-        <v-expansion-panel-title>Operations</v-expansion-panel-title>
-        <v-expansion-panel-text>
-          <v-expansion-panels bg-color="black">
-            <v-expansion-panel v-for="(item, index) in store.$state.operations" :key="index">
-              <v-expansion-panel-title>{{ item.type }}</v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <code class="pre">
-                  {{ item.value }}
+  <div class="flex flex-col gap-3 w-1/2">
+    <s-radio-group v-if="store.$state.operations.length !== 0" v-model="radioState" default-value="formatted" class="flex gap-6">
+      <div class="flex items-center space-x-2">
+        <s-radio-group-item id="formatted" value="formatted" />
+        <s-label for="formatted">
+          Formatted
+        </s-label>
+      </div>
+      <div class="flex items-center space-x-2">
+        <s-radio-group-item id="json" value="json" />
+        <s-label for="json">
+          JSON
+        </s-label>
+      </div>
+      <div class="flex items-center space-x-2">
+        <s-radio-group-item id="binary" value="binary" disabled />
+        <s-label for="binary">
+          Binary
+        </s-label>
+      </div>
+    </s-radio-group>
+    <s-skeleton v-if="store.$state.isLoading" class="w-full h-[100px] skeleton" />
+    <v-expansion-panels v-else bg-color="black" eager>
+      <v-expansion-panel eager>
+        <v-expansion-panel-title>
+          Operations ({{ radioState }})
+        </v-expansion-panel-title>
+        <v-expansion-panel-text eager>
+          <v-expansion-panels bg-color="black" eager>
+            <v-expansion-panel v-for="(item, index) in store.$state.operations" v-show="radioState === 'json'" :key="index" eager>
+              <v-expansion-panel-title>
+                <span class="mr-6">{{ `${index + 1}. ${item.type}` }}</span>
+              </v-expansion-panel-title>
+              <v-expansion-panel-text eager>
+                <code>
+                  {{ JSON.stringify(item.value, null, 2) }}
                 </code>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+            <v-expansion-panel v-for="(item, key) in store.$state.formattedOperations" v-show="radioState === 'formatted'" :key="key" eager>
+              <v-expansion-panel-title>
+                <span class="mr-6">{{ `${key + 1}. ${item.type}` }}</span>
+              </v-expansion-panel-title>
+              <v-expansion-panel-text eager>
+                <component :is="item.value" />
               </v-expansion-panel-text>
             </v-expansion-panel>
           </v-expansion-panels>
@@ -22,6 +55,8 @@
 
 <script lang="ts" setup>
 const store = useWaxStore();
+
+const radioState = ref('formatted');
 </script>
 
 <style scoped>
---
+++ components/ui/OperationsAuthorities.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="w-1/2" :class="store.$state.isLoading ? 'mt-n6' : ''">
+    <h3>
+      Operations Authorities:
+    </h3>
+    <s-skeleton v-if="store.$state.isLoading" class="w-full h-[100px] skeleton" />
+    <div v-for="(item, key) in store.$state.operations" v-else :key="key" class="mt-6">
+      <span v-for="(type, index) in getRequiredAuthorityForOperation(item.type)" :key="index">
+        <span>
+          {{ key + 1 }}.
+          Signed by:
+          <a class="text-blue" :href="`https://explore.openhive.network/@${store.$state.signeesByKeys[key][0]}`">
+            {{ `@${store.$state.signeesByKeys[key][0]}` }}
+          </a>
+        </span>
+        Auth type: <span :class="getColorForType(type)">{{ type }}</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+/* eslint-disable array-callback-return */
+
+const store = useWaxStore();
+
+const getRequiredAuthorityForOperation = (operationType: string): string[] => {
+  const authorityTypes: string[] = [];
+
+  requiredPostingAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Posting');
+  });
+
+  requiredActiveAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Active');
+  });
+
+  requiredOwnerAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Owner');
+  });
+
+  requiredPostingAndActiveAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Posting', 'and', 'Active');
+  });
+
+  requiredPostingOrActiveAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Posting', 'or', 'Active');
+  });
+
+  requiredActiveOrOwnerAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Active', 'or', 'Owner');
+  });
+
+  requiredAnyAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Any');
+  });
+
+  requiredEveryAuthority.some((operation) => {
+    if (operation === operationType)
+      authorityTypes.push('Every');
+  });
+
+  return authorityTypes;
+};
+
+const getColorForType = (type: string): string => {
+  switch (type) {
+    case 'Posting':
+      return 'text-green';
+    case 'Active':
+      return 'text-blue';
+    case 'Owner':
+      return 'text-orange';
+    case 'Any':
+      return 'text-yellow';
+    case 'Every':
+      return 'text-purple';
+    default:
+      return 'gray';
+  }
+};
+</script>
+
+<style scoped>
+.skeleton {
+  background: rgb(63 63 70);
+}
+</style>
