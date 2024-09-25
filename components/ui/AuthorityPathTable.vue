4aebd451202855123a71acb613fef3bd12601d39
fwaszkiewicz <fabianwaszkiewicz09@gmail.com>
2024-09-25 11:23:40 +0200

    feat: Use tooltips from shadcn library

--- components/ui/AuthorityPathTable.vue
+++ components/ui/AuthorityPathTable.vue
@@ -13,47 +13,53 @@
       <s-table-body>
         <s-table-row v-for="(signature, index) in store.signatures.value" :key="index">
           <s-table-cell>
-            <v-tooltip location="top">
-              <template #activator="{props}">
-                <span
-                  v-bind="props"
-                  class="inline-flex items-center transition-colors gap-2 p-3 rounded-lg hover:bg-accent hover:cursor-pointer"
-                  @click="waxStore.copy(signature)"
-                >
-                  <span>
-                    {{ `${signature.slice(0, 5)}...${signature.slice(-5)}` }}
+            <s-tooltip-provider>
+              <s-tooltip>
+                <s-tooltip-trigger as-child>
+                  <span
+                    class="inline-flex items-center transition-colors gap-2 p-3 rounded-lg hover:bg-accent hover:cursor-pointer"
+                    @click="waxStore.copy(signature)"
+                  >
+                    <span>
+                      {{ `${signature.slice(0, 5)}...${signature.slice(-5)}` }}
+                    </span>
+                    <v-icon size="md">mdi-content-copy</v-icon>
                   </span>
-                  <v-icon size="md">mdi-content-copy</v-icon>
-                </span>
-              </template>
-              <div class="flex flex-col">
-                <span class="text-lg">Signature:</span>
-                <hr class="my-2">
-                <span>{{ signature }}</span>
-              </div>
-            </v-tooltip>
+                </s-tooltip-trigger>
+                <s-tooltip-content>
+                  <div class="flex flex-col">
+                    <span class="text-lg">Signature:</span>
+                    <hr class="my-2">
+                    <span>{{ signature }}</span>
+                  </div>
+                </s-tooltip-content>
+              </s-tooltip>
+            </s-tooltip-provider>
           </s-table-cell>
           <s-table-cell>
             {{ store.pack.value }}
           </s-table-cell>
           <s-table-cell>
-            <v-tooltip location="top">
-              <template #activator="{props}">
-                <span
-                  v-bind="props"
-                  class="inline-flex items-center transition-colors gap-2 p-3 rounded-lg hover:bg-accent hover:cursor-pointer"
-                  @click="waxStore.copy(store.publicKeys.value[index])"
-                >
-                  <span>{{ `${store.publicKeys.value[index].slice(0, 5)}...${store.publicKeys.value[index].slice(-5)}` }}</span>
-                  <v-icon size="md">mdi-content-copy</v-icon>
-                </span>
-              </template>
-              <div class="flex flex-col">
-                <span class="text-lg">Public key:</span>
-                <hr class="my-2">
-                <span>{{ store.publicKeys.value[index] }}</span>
-              </div>
-            </v-tooltip>
+            <s-tooltip-provider>
+              <s-tooltip>
+                <s-tooltip-trigger as-child>
+                  <span
+                    class="inline-flex items-center transition-colors gap-2 p-3 rounded-lg hover:bg-accent hover:cursor-pointer"
+                    @click="waxStore.copy(store.publicKeys.value[index])"
+                  >
+                    <span>{{ `${store.publicKeys.value[index].slice(0, 5)}...${store.publicKeys.value[index].slice(-5)}` }}</span>
+                    <v-icon size="md">mdi-content-copy</v-icon>
+                  </span>
+                </s-tooltip-trigger>
+                <s-tooltip-content>
+                  <div class="flex flex-col">
+                    <span class="text-lg">Public key:</span>
+                    <hr class="my-2">
+                    <span>{{ store.publicKeys.value[index] }}</span>
+                  </div>
+                </s-tooltip-content>
+              </s-tooltip>
+            </s-tooltip-provider>
           </s-table-cell>
           <s-table-cell>
             <span v-for="(item, key) in store.authorityPath.value" :key="key">
