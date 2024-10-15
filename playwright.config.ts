5d40f78c6d149957f0dc8969b4bcc0d400655e7f
Bartek Wrona <wrona@syncad.com>
2024-10-16 01:34:40 +0200

    playwright.config extended by passing custom mock data file path

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,8 @@
 // This is a workaround for https://github.com/microsoft/playwright/issues/18282#issuecomment-1612266345
 import { defineConfig } from '@playwright/test';
+import { type ITxInspectorTest } from './__tests__/assets/jest-helper.js';
 
-export default defineConfig({
+export default defineConfig < ITxInspectorTest> ({
   reporter: [
     ['junit', { outputFile: 'results.xml' }],
     ['json', { outputFile: 'results.json' }]
@@ -9,7 +10,12 @@ export default defineConfig({
   projects: [
     {
       name: 'tx_inspector_testsuite',
-      testDir: './__tests__/'
+      testDir: './__tests__/',
+      use:
+      {
+        fixtureLevelMockFile: 'Ala ma kota.json'
+      }
+
     }
   ],
   // Run your local dev server before starting the tests
