2e2aa7a74096d5f528ccd5edadcf1f51cd8b81ff
fwaszkiewicz <fabianwaszkiewicz09@gmail.com>
2025-03-21 10:24:34 +0100

    style: Improve select group width and spacings

--- components/shadcn/select/SelectGroup.vue
+++ components/shadcn/select/SelectGroup.vue
@@ -13,7 +13,7 @@ const delegatedProps = computed(() => {
 </script>
 
 <template>
-  <SelectGroup :class="cn('p-1 w-full', props.class)" v-bind="delegatedProps">
+  <SelectGroup :class="cn('p-1 -ml-1 w-[104%]', props.class)" v-bind="delegatedProps">
     <slot />
   </SelectGroup>
 </template>
